e1e4696374c4034ea710cf983eac1ad3ade241e7
Brian Thornburg <briant3275@gmail.com>
2022-01-14 00:50:58 -0600

    first attempt at sort challenges

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -84,6 +84,9 @@ const greeting = (word) => {
 
 const speaker = (words, callback) => {
   // Solution code here...
+  let newArr = callback(words);
+
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -190,7 +193,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add the number 8 to the array five times', () => {
     expect(addNumbers(8, [], 5, addValues)).toStrictEqual([8, 8, 8, 8, 8]);
     expect(addNumbers(8, [], 5, addValues).length).toStrictEqual(5);
